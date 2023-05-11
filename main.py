4378dc8b613ecd8d6f114c485401989abe2d565e
godfather <maks12ate@gmail.com>
2023-05-11 15:54:05 +0300

    add .gitignore and requirements.txt

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from objects import *
 import keyboard
 import os
-from time import sleep
 
-clear = lambda: os.system('cls')
+from time import sleep
+from sys import platform
+
+from objects import *
+
+clear = lambda: os.system('cls') if platform == 'win32' else os.system('clear')
